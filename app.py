85d0876ef649c36a3f1971203283b383f62a7870
Elijah Christensen <ejd.christensen@gmail.com>
2023-12-19 09:23:54 -0700

    working version w/ sample

--- app.py
+++ app.py
@@ -4,32 +4,33 @@ import streamlit as st
 import calmap
 
 # Upload XLS data
-with st.sidebar.header('1. Upload your caselog xls  data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input caselog excel file", type=["xls"])
+with st.sidebar.header('1. Upload your caselog'):
+    uploaded_file = st.sidebar.file_uploader("Upload ADS case log (xls)", type=["xls"])
     st.sidebar.markdown("""
-[Example XLS input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-""")
+        [Example XLS input file](https://github.com/elijahc/st_ads_profiler/raw/master/ref/ACLResProcDetail-edc-sample.xls)
+        """)
+
+with st.sidebar.header('2. Apply filters'):
+    pass
 
 if uploaded_file is not None:
     @st.cache_data
     def load_xls():
         xls_df = pd.read_excel(uploaded_file,skiprows=10)
         last_row = xls_df.index[xls_df[xls_df.columns[-1]].str.startswith('Case Total')==True].tolist()
-        xls_df = xls_df.iloc[:last_row[0]-1].reset_index(drop=True)
+        last_row = last_row[0]-1
+        xls_df = xls_df.iloc[:last_row].reset_index(drop=True)
         xls_df = xls_df.dropna(axis=1,how='all')
+        xls_df = xls_df.dropna(axis=0,how='all')
         xls_df = xls_df.rename(columns={k:v for k,v in zip(xls_df.columns,['attr','value','area-val'])})
         # case_idxs = df.attr=='Case Date:'        
         return xls_df 
-
     df = load_xls()
 
     def extract_log_meta(df):
         df_dict = df.iloc[:6,:2].to_dict()
         df_dict = {k:v for k,v in zip(df_dict['attr'].values(),df_dict['value'].values())}
         return pd.Series(df_dict)
-
-    def get_log_meta(df):
-        return log_meta
 
     def process_xls(df):
         case_idxs = df.attr=='Case Date:'
@@ -60,6 +61,8 @@ if uploaded_file is not None:
         
         return fig,axs
 
+    st.header('**Input DataFrame**')
+    st.write(df)
 
     lm, lma = process_xls(df)
     # pr = ProfileReport(df, explorative=True)
@@ -69,8 +72,5 @@ if uploaded_file is not None:
 
     st.write('---')
 
-    st.header('**Input DataFrame**')
-    st.write(df)
-    
     st.header('**Processed Log**')
     st.write(lma)
